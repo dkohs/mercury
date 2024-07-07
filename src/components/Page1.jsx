97dfcd5911eb42d64f72a1e30c2bdd434bdc2a0a
phonelaptop <jakeyjkim@gmail.com>
2024-07-07 11:30:29 -0700

    Changes to Page 1 css

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,20 +1,27 @@
+import heartIcon from "../assets/img/women_smiles.png";
+
 export const Page1 = () => {
   return (
-    <div>
-      <div className={"page1-subtitle"}>WELCOME TO</div>
-      <div className={"page1-subtitle"}>
-        CARDIOVASCUAR DISEASE PREDICTIVE ANALYSIS
-      </div>
+    <div className={"page1"}>
       <div className={"page1-text"}>
-        In a land where the sun shone a gentle lavender hue and the rivers
-        flowed with liquid gold, there lived a community of sentient mushrooms.
-        These mushrooms, with caps adorned in intricate patterns of silver and
-        emerald, communicated through a symphony of bioluminescent flickers.
-        Each flicker told a story of ancient forest magic, the kind that made
-        the trees hum with life and the air shimmer with mystery. In the heart
-        of this enchanted forest stood the Great Spore Tree, whose branches
-        reached towards the stars, and whose roots whispered secrets to the
-        earth.
+        <div className={"page1-subtitle"}>WELCOME TO</div>
+        <div className={"page1-subtitle2"}>
+          CARDIOVASCUAR DISEASE PREDICTIVE ANALYSIS
+        </div>
+        <div className={"page1-bodytext"}>
+          In a land where the sun shone a gentle lavender hue and the rivers
+          flowed with liquid gold, there lived a community of sentient
+          mushrooms. These mushrooms, with caps adorned in intricate patterns of
+          silver and emerald, communicated through a symphony of bioluminescent
+          flickers. Each flicker told a story of ancient forest magic, the kind
+          that made the trees hum with life and the air shimmer with mystery. In
+          the heart of this enchanted forest stood the Great Spore Tree, whose
+          branches reached towards the stars, and whose roots whispered secrets
+          to the earth.
+        </div>
+      </div>
+      <div className={"women-smiles"}>
+        <img src={heartIcon} alt={"women-smiles"} />
       </div>
     </div>
   );
