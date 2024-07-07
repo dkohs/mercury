ac50d7e6ef08160cecb98dc74efe22e42defbf3b
phonelaptop <jakeyjkim@gmail.com>
2024-07-07 10:36:46 -0700

    page1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { NavBar} from "./components/NavBar"
 import { About } from "./components/About"
+import { Page1 } from "./components/Page1"
+
+
+
 
 export const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <About/>
+      <About />
+      <Page1 />
     </div>
   );
 }
