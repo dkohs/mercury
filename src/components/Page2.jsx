af155441a26a8af93c014562d577e00165fd9cb5
DannyDopestar <danielsuh39@gmail.com>
2024-07-06 21:42:13 -0700

    Page 2 Forms

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,3 +1,30 @@
 export const Page2 = () => {
-    return (<div>Daniel</div>)
+    return (<div>
+        <div>Daniel</div>
+        <div classname="page2-subtitle1">
+            <nav class="button-bar">
+                <h1>CARDIOVASCULAR DISEASE PREDICTIVE ANALYSIS</h1>
+                <button>Main</button>
+                <button>About</button>
+                <button>Contact</button>
+            </nav>
+            <div>_______________________________________________________</div>
+    </div>
+        
+        
+    <body style="background-color: lightgray;"> 
+   
+        <form>
+            <div id = "form title">FILL IN THE FOLLOWING INFORMATION</div>
+            <input type="text">Full Name</input>
+            <input type="text">Age</input>
+            <input type="text">Smoking History</input>
+            <input type="text">Please add any additional information</input>
+            <button>SUBMIT</button>
+            </form>
+            
+
+    </body>
+    </div>
+    )
 }
