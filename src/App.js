a379f6997071828e700f0f7d12f7473f10c6bc49
DannyDopestar <danielsuh39@gmail.com>
2024-07-07 10:28:57 -0700

    Initial Styling for Page2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { NavBar} from "./components/NavBar"
-import { About } from "./components/About"
+import { NavBar } from "./components/NavBar";
+import { About } from "./components/About";
+import { Page2 } from "./components/Page2";
 
 export const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <About/>
+      <About />
+      <Page2 />
     </div>
   );
-}
+};
--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,30 +1,27 @@
 export const Page2 = () => {
-    return (<div>
-        <div>Daniel</div>
-        <div classname="page2-subtitle1">
-            <nav class="button-bar">
-                <h1>CARDIOVASCULAR DISEASE PREDICTIVE ANALYSIS</h1>
-                <button>Main</button>
-                <button>About</button>
-                <button>Contact</button>
-            </nav>
-            <div>_______________________________________________________</div>
-    </div>
-        
-        
-    <body style="background-color: lightgray;"> 
-   
-        <form>
-            <div id = "form title">FILL IN THE FOLLOWING INFORMATION</div>
-            <input type="text">Full Name</input>
-            <input type="text">Age</input>
-            <input type="text">Smoking History</input>
-            <input type="text">Please add any additional information</input>
-            <button>SUBMIT</button>
-            </form>
-            
+  return (
+    <div>
+      <div>Daniel</div>
+      <div className="page2-subtitle1">
+        <nav className="button-bar">
+          <h1>CARDIOVASCULAR DISEASE PREDICTIVE ANALYSIS</h1>
+          <button>Main</button>
+          <button>About</button>
+          <button>Contact</button>
+        </nav>
+        <div>_______________________________________________________</div>
+      </div>
 
-    </body>
+      <div style={{"backgroundColor": "lightgray"}}>
+        <form>
+          <div id="form title">FILL IN THE FOLLOWING INFORMATION</div>
+        <input type="text" value="Full Name"/>
+          <input type="text" value="Age"/>
+          <input type="text" value="Smoking History"/>
+          <input type="text" value="Please Add Any Additional Information"/>
+          <button>SUBMIT</button>
+        </form>
+      </div>
     </div>
-    )
-}
+  );
+};
