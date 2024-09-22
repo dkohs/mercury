142100cb9f5258535a8c5a86a7a42a9278e17267
phonelaptop <jakeyjkim@gmail.com>
2024-09-22 10:12:51 -0700

    ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
-import { NavBar } from "./components/NavBar"
-import { Hero } from "./components/Hero"
-import { About } from "./components/About"
-import { CardioForm } from "./components/CardioForm"
-import { Footer } from "./components/Footer"
+import { NavBar } from "./components/NavBar";
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { CardioForm } from "./components/CardioForm";
+import { Footer } from "./components/Footer";
+import { LogInForm } from "./components/LogInForm";
+import { SignUpForm } from "./components/SignUpForm";
 import { Box } from "@mui/material";
-
 
 export const App = () => {
   return (
     <Box className="App">
       <NavBar />
-      <Hero/>
+      <Hero />
       <About />
       <CardioForm />
+      <LogInForm />
+      <SignUpForm />
       <Footer />
     </Box>
   );
---
+++ client/src/components/SignUpForm.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { TextField, Button, Box, Typography } from "@mui/material";
+
+export const SignUpForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "300px",
+        margin: "auto",
+        padding: "20px",
+        boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
+        borderRadius: "8px",
+        backgroundColor: "#f9f9f9",
+      }}
+    >
+      <Typography variant="h5" sx={{ marginBottom: "20px" }}>
+        Sign up
+      </Typography>
+          <Box>
+        <TextField
+          label="First Name"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Last Name"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+      </Box>
+      <TextField
+        label="Email"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button
+        type="Sign Up"
+        variant="contained"
+        color="primary"
+        fullWidth
+        sx={{
+          marginTop: "20px",
+        }}
+      >
+        Sign Up
+      </Button>
+      <Box
+        sx={{
+          height: "2px",
+          width: "250px",
+          backgroundColor: "#E5CCCC", // Line color
+          marginX: "16", // Margin around the line
+          marginTop: "20px",
+        }}
+      />
+      <Typography
+        variant="h6"
+        sx={{
+          marginTop: "15px",
+          fontSize: "15px",
+        }}
+      >
+        Create an Account
+      </Typography>
+    </Box>
+  );
+};
+
+export default SignUpForm;
